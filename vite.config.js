ad50c2e36efed1ed8b5a3a6389ebf1814886d068
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-16 02:30:02 +0700

    Clean-up

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { v4wp } from "@kucrut/vite-for-wp";
 
 // See https://vitejs.dev/config/
 export default {
-  plugins: [v4wp({input: "app/src/main.js", outDir: "app/dist"})]
+	plugins: [v4wp({ input: "app/src/main.js", outDir: "app/dist" })],
 };
